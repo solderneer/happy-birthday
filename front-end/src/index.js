122f8f8cf73482a7955ce331c4f1f7f20ef0b34e
Sudharshan <sudhar393@gmail.com>
2018-05-02 21:20:30 +0800

    made some progress on the UI

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,76 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typing from 'react-typing-animation';
 import './index.css';
 
-function Square(props) {
+class CountDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bdate: new Date("June 8, 2018 00:00:00").getTime(),
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  commponentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    let now = new Date().getTime();
+    let distance = this.state.bdate - now;
+    this.setState({
+      days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((distance % (1000 * 60)) / 1000)
+    });
+  }
+
+  render() {
     return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
+      <div className="jumbotron">
+        <div className="container">
+          <span className="countdown big">{this.state.days}</span>
+          <span className="countdown small">d</span><span className="big"> </span>
+          <span className="countdown big">{this.state.hours}</span>
+          <span className="countdown small">h</span><span className="big"> </span>
+          <span className="countdown big">{this.state.minutes}</span>
+          <span className="countdown small">min</span><span className="big"> </span>
+          <span className="countdown big">{this.state.seconds}</span>
+          <span className="countdown small">s</span><span className="big"> </span>
+        </div>
+      </div>
     );
+  }
 }
-
-class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: Array(9).fill(null)};
-    }
-
-    handleClick(i) {
-        const squares = this.state.value.slice();
-        squares[i] = 'X';
-        this.setState({value: squares});
-    }
-
-    renderSquare(i) {
-        return <Square value={this.state.value[i]} onClick={() => this.handleClick(i)}/>;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-            <div className="game-board">
-            <Board />
-            </div>
-            <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-            </div>
-            </div>
-        );
-    }
-}
-
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <CountDown />,
     document.getElementById('root')
 );
